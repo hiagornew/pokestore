aeae6cdfac9865a87bc1f7c6ab44f33d1f8653a8
Hiagor Queiros de Oliveira <hiagornew@gmail.com>
2020-07-02 15:12:03 -0300

    busca pokmons

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,38 +1,49 @@
-import React, { useState } from 'react';
+import React, { useState,useEffect } from 'react';
 import axios from 'axios';
 import * as S from '../Components/styled';
 import { useHistory } from 'react-router-dom';
 
-function App() {
+const  Home = ()=> {
   const history = useHistory();
   const [usuario, setUsuario] = useState('');
   const[erro, setErro] =  useState(false);
-
-  function HandlePesquisa(){
+  const[repositoriesName, setRepositoriesName] = useState([])
+  const HandlePesquisa = () =>{
     //acessando a api com axios get
-    axios.get(`https://api.github.com/users/${usuario}/repos`).then(response => {
-      const repositories = response.data;
-      const repositoriesName =[];
+    axios.get(`https://pokeapi.co/api/v2/pokemon/`).then(response => {
+      const repositories = response.data.results;
+       const responseNames =[];
       repositories.map((repository) =>{
-        repositoriesName.push(repository.name);
+        responseNames.push(repository.name);
+       
       });
+      setRepositoriesName(responseNames)
+      //console.log(repositoriesName);
       //salvando no Storage
-      localStorage.setItem('repositoriesName', JSON.stringify(repositoriesName));
-      localStorage.setItem('user',usuario);
+      //localStorage.setItem('repositoriesName', JSON.stringify(repositoriesName));
+      //localStorage.setItem('user',usuario);
       //mudando para a pagina repositories
-      setErro(false);
-      history.push('/repositories')
+      //setErro(false);
+     // history.push('/repositories')
     }).catch(err =>{
       setErro(true);
     });
   }
 
+useEffect(()=>{
+  HandlePesquisa()
+},[])
+
 
   return (
+    
     <S.ContainerHome>
+       { repositoriesName.map(name =>{
+        return<h1>{name}</h1>
+      })}
       <S.Content>
       <S.Input  className="usuarioInput" placeholder="Usuário" value={usuario} onChange={e => setUsuario(e.target.value)} />
-      <S.Button type="button" onClick={HandlePesquisa} >Pesquisar</S.Button>
+    
       
       </S.Content>
       {erro ? <S.ErrorMsg>Ocorreu um erro, Tente Novamente</S.ErrorMsg> : ''}
@@ -40,4 +51,4 @@ function App() {
   );
 }
 
-export default App;
+export default Home;
